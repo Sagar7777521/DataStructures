ba12ba121ffbdfa4aae31cc83b003407c0441159
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-17 13:36:55 +0530

    Initiated

---
+++ index.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+  enqueue(element) {
+    this.items.push(element);
+  }
+  dequeue(element) {
+    return this.items.shift(element);
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[0];
+    }
+    return null;
+  }
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+
+console.log(queue.size());
+console.log(queue.print());
+console.log(queue.dequeue());
+console.log(queue.print());
