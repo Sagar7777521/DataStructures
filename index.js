f39d7a2bdf35851ba8762135cca7f9c28a86e782
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-18 20:36:41 +0530

    Linked List

--- index.js
+++ index.js
@@ -90,84 +90,181 @@
 
 // ********************* Circular Queue Implementation ****************
 
-class CircularQueue {
-  constructor(capacity) {
-    this.items = new Array(capacity);
-    this.capacity = capacity;
-    this.currentLength = 0;
-    this.rear = -1;
-    this.front = -1;
-  }
+// class CircularQueue {
+//   constructor(capacity) {
+//     this.items = new Array(capacity);
+//     this.capacity = capacity;
+//     this.currentLength = 0;
+//     this.rear = -1;
+//     this.front = -1;
+//   }
 
-  isFull() {
-    return this.currentLength == this.capacity;
+//   isFull() {
+//     return this.currentLength == this.capacity;
+//   }
+
+//   isEmpty() {
+//     return this.currentLength === 0;
+//   }
+//   enqueue(element) {
+//     if (!this.isFull()) {
+//       this.rear = this.rear + 1;
+//       this.items[this.rear] = element;
+//       this.currentLength += 1;
+//       if (this.front === -1) {
+//         this.front = this.rear;
+//       }
+//     }
+//   }
+//   dequeue() {
+//     if (this.isEmpty()) {
+//       return null;
+//     }
+//     const item = this.items[this.front];
+//     this.items[this.front] = null;
+//     this.front = (this.front + 1) % this.capacity;
+//     this.currentLength -= 1;
+//     if (this.isEmpty()) {
+//       this.front = -1;
+//       this.rear = -1;
+//     }
+//     return item;
+//   }
+
+//   peek() {
+//     if (!this.isEmpty()) {
+//       return this.items[this.front];
+//     }
+//     return null;
+//   }
+
+//   print() {
+//     if (this.isEmpty()) {
+//       console.log("Queue is empty");
+//     } else {
+//       let i;
+//       let str = "";
+//       for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+//         str += this.items[i] + " ";
+//       }
+//       str += this.items[i];
+//       console.log(str);
+//     }
+//   }
+// }
+
+// const queue = new CircularQueue(5);
+
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// queue.enqueue(40);
+// queue.enqueue(50);
+
+// // console.log(queue.isEmpty());
+// // queue.print();
+// // console.log(queue.isFull());
+// // queue.dequeue();
+// // queue.print();
+// queue.dequeue();
+// queue.print();
+
+// queue.enqueue(70);
+// queue.print();
+
+// *******************Linked List ******************************
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
   }
 
   isEmpty() {
-    return this.currentLength === 0;
-  }
-  enqueue(element) {
-    if (!this.isFull()) {
-      this.rear = this.rear + 1;
-      this.items[this.rear] = element;
-      this.currentLength += 1;
-      if (this.front === -1) {
-        this.front = this.rear;
-      }
-    }
-  }
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    const item = this.items[this.front];
-    this.items[this.front] = null;
-    this.front = (this.front + 1) % this.capacity;
-    this.currentLength -= 1;
-    if (this.isEmpty()) {
-      this.front = -1;
-      this.rear = -1;
-    }
-    return item;
+    return this.size === null;
   }
 
-  peek() {
-    if (!this.isEmpty()) {
-      return this.items[this.front];
+  getSize() {
+    return this.size;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
-    return null;
+    this.size++;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
   }
 
   print() {
     if (this.isEmpty()) {
-      console.log("Queue is empty");
+      console.log("List is Empty");
     } else {
-      let i;
-      let str = "";
-      for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
-        str += this.items[i] + " ";
+      let curr = this.head;
+      let listValues = " ";
+      while (curr) {
+        listValues += `${curr.value}`;
+        curr = curr.next;
       }
-      str += this.items[i];
-      console.log(str);
+      console.log(listValues.trim());
+    }
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      console.log("Invalid Entry");
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
     }
   }
 }
 
-const queue = new CircularQueue(5);
+const list = new LinkedList();
 
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-queue.enqueue(40);
-queue.enqueue(50);
-
-// console.log(queue.isEmpty());
-// queue.print();
-// console.log(queue.isFull());
-// queue.dequeue();
-// queue.print();
-queue.dequeue();
-queue.print();
-
-queue.enqueue(70);
-queue.print();
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.prepend(40);
+list.prepend(50);
+list.print();
+console.log("Total Size of list", list.getSize());
+list.append(100);
+list.print();
+list.insert(60, 3);
+list.print();
+list.insert(121, 1);
+list.print();
